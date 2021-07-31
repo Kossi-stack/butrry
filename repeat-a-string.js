f1d69189f420e295e73a4315a5ad0cf1c1f780c1
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-07-31 07:08:50 -0700

    solve

---
+++ repeat-a-string.js
@@ -0,0 +1,13 @@
+module.exports = function repeatStringNumTimes (str, num) {
+    if ( num <= 0){
+      return "";
+    }
+    let result = "";
+  
+    for (let i = 0; i < num; i += 1) {
+      result += str
+    }
+    return result;
+  }
+
+  repeatStringNumTimes("abc", 3);
